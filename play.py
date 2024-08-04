3759cc46e56a33241cbaae83a8d8f2ee918b2466
Yoad Hordan <yoad.hordan@i-feelbetter.com>
2024-08-04 12:29:58 +0300

    will this print file

--- play.py
+++ play.py
@@ -6,6 +6,10 @@ def multi(a, b):
 
 def main():
     print("hello world2")
+    with open('RandomThing.txt') as file:
+        print(file.read())
+        print("-----")
+        # print(file.readline())
 
 
 if __name__ == '__main__':
