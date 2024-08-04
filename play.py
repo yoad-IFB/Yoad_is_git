e66c557fba5ba438ae85884aceafbe608e94360e
Yoad Hordan <yoad.hordan@i-feelbetter.com>
2024-08-04 14:02:13 +0300

    suf multi

--- play.py
+++ play.py
@@ -1,16 +1,22 @@
 
 
-def multi(a, b):
-    return a * b
+def multi():
+    with open('RandomThing.txt') as file:
+        content = file.readlines()
+        return int(content[3]) * int(content[4])
+
+
+def print_file():
+    with open('RandomThing.txt') as file:
+        print(file.read())
 
 
 def main():
     print("hello world2")
-    with open('RandomThing.txt') as file:
-        print(file.read())
-        print("-----")
-        # print(file.readline())
-    print(multi(5, 6))
+    print_file()
+    print("-----")
+    # print(file.readline())
+    print(multi())
 
 
 if __name__ == '__main__':
